ef1abac8ad1e71fcb59c5998bc91f299166dbfc0
eSadikovic <emir.sadikovic10@gmail.com>
2017-07-05 12:08:37 +0200

    Update ClassifySVM.py

--- ClassifySVM.py
+++ ClassifySVM.py
@@ -9,8 +9,8 @@ def classify(features_train, labels_train):
     
     
     from sklearn import svm
-    ce=1
-    gm=100
+    ce=13
+    gm=1
     clf = svm.SVC(kernel="rbf", gamma=gm, C=ce)
     clf.fit(features_train, labels_train)
     return [clf, ce, gm]
