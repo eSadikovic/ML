f33b45bc4f227ba8f9c30a9b8052a67a87667457
eSadikovic <emir.sadikovic10@gmail.com>
2017-07-25 13:07:43 +0200

    Update ClassifySVM.py

--- ClassifySVM.py
+++ ClassifySVM.py
@@ -1,11 +1,5 @@
 def classify(features_train, labels_train):   
-    ### import the sklearn module for GaussianNB
-    ### create classifier
-    ### fit the classifier on the training features and labels
-    ### return the fit classifier
-    
-    
-    ### your code goes here!
+
     
     
     from sklearn import svm
